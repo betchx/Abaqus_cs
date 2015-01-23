bde2df4b64fef99f0383c5e4940c04f03fbc21d2
Atsushi TANABE <tanabe.atsushi@nikken.jp>
2015-01-23 17:36:42 +0900

    比較できないためにエラーとなっていたのを修正
    IComparableを実装

--- AbaqusModel/Address.cs
+++ AbaqusModel/Address.cs
@@ -5,14 +5,43 @@ using System.Text;
 
 namespace Abaqus
 {
-    public struct Address 
+    public struct Address : IComparable
     {
-        string name;
-        uint id;
-        public Address(uint id)
+        public string name;
+        public uint id;
+        public Address(string name, uint id)
         {
-            name = "";
+            this.name = name;
             this.id = id;
         }
+        public Address(uint id)
+            : this("", id)
+        {
+        }
+
+        public int CompareTo(Object obj)
+        {
+            if (obj is Address)
+            {
+                var other = (Address)obj;
+                var res = name.CompareTo(other.name);
+                if (res == 0)
+                {
+                    res = id.CompareTo(other.id);
+                }
+                return res;
+            }
+            if (obj is uint)
+            {
+                return CompareTo(new Address("",(uint)obj));
+            }
+            return -1;
+        }
+
+        public static implicit operator Address(uint id)
+        {
+            return new Address(id);
+        }
+
     }
 }
