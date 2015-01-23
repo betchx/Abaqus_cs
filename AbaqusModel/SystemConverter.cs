e7b4cd54d9061a990250003fc0bde1faa616fcfb
Atsushi TANABE <tanabe.atsushi@nikken.jp>
2015-01-23 11:45:57 +0900

    座標変換登録をリファクタリングして整理．
    局所座標系を使う場合をメソッドとして切り出して再利用．

--- AbaqusModel/SystemConverter.cs
+++ AbaqusModel/SystemConverter.cs
@@ -22,9 +22,9 @@ namespace Abaqus
         ///  並進のみ
         /// </summary>
         /// <param name="origin">ローカル座標の原点</param>
-        public SystemConverter(Point3D origin)
+        public SystemConverter(Point3D origin): this()
         {
-            transform = new Transform3DGroup();
+            // 並進はわざわざマトリックスを作成するまでもないので，直接作成する．
             transform.Children.Add(new TranslateTransform3D(origin.X, origin.Y, origin.Z));
         }
 
@@ -34,27 +34,16 @@ namespace Abaqus
         /// </summary>
         /// <param name="origin">ローカル座標の原点</param>
         /// <param name="xaxis">ローカルX軸上の点</param>
-        public SystemConverter(Point3D origin, Point3D xaxis)
+        public SystemConverter(Point3D origin, Point3D xaxis):this()
         {
-            transform = new Transform3DGroup();
+            // XY平面の指定がない場合，局所Z軸はグローバルと同じになる．
+            var w = new Vector3D(0, 0, 1);
+            // 局所座標系の軸ベクトルを作成
+            var u = xaxis - origin;
+            var v = Vector3D.CrossProduct(w, u);
 
-            // 準備
-            var g_x = new Vector3D(1, 0, 0);
-            var g_z = new Vector3D(0, 0, 1);
-            var l_x = xaxis - origin;
-            l_x.Normalize();
-
-            // 回転角度 0～180°
-            var dot = Vector3D.DotProduct(g_x, l_x);
-            var angle = Math.Acos( dot ) * 180.0 / Math.PI;
-            // 回転軸 ＝＞ 回転方向．  Acosで算出している関係で回転方向がなくなるので，外積で算出する．
-            var ax = (dot == -1.0)?g_z:Vector3D.CrossProduct(g_x, l_x);
- 
-            // グローバルZ軸まわりの回転を追加
-            var rot = new AxisAngleRotation3D(ax, angle);
-            transform.Children.Add(new RotateTransform3D(rot));
-            // 並進を追加
-            transform.Children.Add(new TranslateTransform3D(origin.X, origin.Y, origin.Z));
+            // 登録
+            RegisterTransform(origin, u, v, w);
         }
 
         /// <summary>
@@ -63,17 +52,31 @@ namespace Abaqus
         /// <param name="origin">ローカル座標の原点</param>
         /// <param name="xaxis">ローカルX軸上の点</param>
         /// <param name="xyplain">ローカルXY平面上でY軸が正の方向にある点</param>
-        public SystemConverter(Point3D origin, Point3D xaxis, Point3D xyplain)
+        public SystemConverter(Point3D origin, Point3D xaxis, Point3D xyplain) : this()
         {
-            transform = new Transform3DGroup();
-
+            // 局所座標系の軸ベクトルを作成
             var u = xaxis - origin;
-            u.Normalize();
             var w = Vector3D.CrossProduct(u, xyplain - origin);
-            w.Normalize();
             var v = Vector3D.CrossProduct(w, u);
-            v.Normalize();
 
+            // 登録
+            RegisterTransform(origin, u, v, w);
+        }
+
+
+        /// <summary>
+        ///   局所座標系の原点とベクトルから変換を作成して登録
+        /// </summary>
+        /// <param name="origin">局所座標系の原点</param>
+        /// <param name="u">局所X軸ベクトル</param>
+        /// <param name="v">局所Y軸ベクトル</param>
+        /// <param name="w">局所Z軸ベクトル</param>
+        private void RegisterTransform(Point3D origin, Vector3D u, Vector3D v, Vector3D w)
+        {
+            // ベクトルを正規化
+            u.Normalize();
+            w.Normalize();
+            v.Normalize();
             // 変換マトリックスの作成
             var mat = new Matrix3D(
                         u.X, u.Y, u.Z, 0.0,
@@ -84,6 +87,7 @@ namespace Abaqus
             // 登録
             transform.Children.Add(new MatrixTransform3D(mat));
         }
+
 
     }
 }
