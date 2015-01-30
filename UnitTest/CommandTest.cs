7059ae1ad9fd9f8dbe7be7373fa611b23e592c6f
Atsushi TANABE <tanabe.atsushi@nikken.jp>
2015-01-31 00:02:57 +0900

    コマンドのテストを強化
    must_have

--- UnitTest/CommandTest.cs
+++ UnitTest/CommandTest.cs
@@ -22,23 +22,46 @@ namespace UnitTest
         {
             get
             {
+                // 小文字
                 var cmd = new Command("element");
                 yield return cmd;
-                
+
+                // 大文字
                 cmd.keyword = Abaqus.Keyword.ASSEMBLY;
                 yield return cmd;
-                
+
+                // ELSET
                 var elset = new Command(Keyword.ELSET);
                 elset.parameters.Add("ELSET", "ALL");
                 elset.datablock.Add("1, 2, 3");
                 yield return elset;
 
+                // Element
                 var elm = new Command(Keyword.ELEMENT);
                 elm.parameters.Add("TYPE", "S4R");
                 elm.parameters.Add("ELSET", "SHELL");
                 elm.datablock.Add("1, 1, 2, 3, 4");
                 yield return elm;
 
+                // Node
+                var node = new Command(Keyword.NODE);
+                node.datablock.Add("1");
+                yield return node;
+                node.datablock.Add("2,1");
+                yield return node;
+                node.datablock.Add("3,2.,3.");
+                yield return node;
+                node.datablock.Add("4,3.,3.,1");
+                yield return node;
+
+                // 存在しないキーワード
+                var not_exist = new Command("hogehoge");
+                not_exist.datablock.Add("not exist");
+                yield return not_exist;
+                not_exist.parameters.Add("FOO", "bar");
+                yield return not_exist;
+                not_exist.parameters.Add("part", "");
+                yield return not_exist;
 
             }
         }
@@ -118,8 +141,25 @@ namespace UnitTest
         }
 
 
+        [Theory]
+        public void must_haveは所定の名前のパラメータを有する場合例外をスローしない(
+            Command cmd,
+            string param_name)
+        {
+            Assume.That(cmd.parameters.Keys, Has.Member(param_name.ToUpper()));
+            //Assert.Fail();
+            Assert.DoesNotThrow(() => {cmd.must_have(param_name);});
+        }
 
-
+        [Theory]
+        public void must_haveは所定の名前のパラメータを有しない場合InvalidFormatExceptionをスローする(
+            Command cmd,
+            string param_name)
+        {
+            Assume.That(cmd.parameters.Keys, Has.No.Member(param_name.ToUpper()));
+            //Assert.Fail();
+            Assert.Throws<InvalidFormatException>(() => { cmd.must_have(param_name); });
+        }
 
     }
 }
